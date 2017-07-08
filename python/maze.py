08eb774db1fbb0ff78b1cd49ce9bcca4ae1dd85f
Daniel Fuller <lasaldan@gmail.com>
2017-07-08 09:58:05 -0600

    Python Maze Solver

---
+++ python/maze.py
@@ -0,0 +1,47 @@
+maze = [
+  [0,0,0,0,0,0,0,0,0,0],
+  [1,1,0,1,1,1,1,0,1,0],
+  [0,1,0,1,0,0,1,0,1,0],
+  [0,1,1,1,0,1,1,0,1,0],
+  [0,1,0,1,0,1,0,0,1,0],
+  [0,1,0,1,0,1,1,1,1,0],
+  [0,1,0,0,0,0,0,1,0,0],
+  [0,1,0,1,1,1,0,1,0,0],
+  [0,1,1,1,0,1,0,1,1,1],
+  [0,0,0,0,0,0,0,0,0,0]
+]
+
+row = 1
+col = 0
+
+finishRow = 8
+finishCol = 9
+
+def solve(row, col):
+
+    if maze[row][col] == 0 or maze[row][col] == 2:
+        return False
+
+    if row == finishRow and col == finishCol:
+        return True
+
+    maze[row][col] = 2
+
+    if solve(row-1, col):
+        return True
+
+    if solve(row, col+1):
+        return False
+
+    if solve(row+1, col):
+        return True
+
+    if solve(row, col-1):
+        return True
+
+    maze[row][col] = 1
+
+    return False
+
+solve(row, col)
+print maze
